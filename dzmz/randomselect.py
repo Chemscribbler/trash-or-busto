34397080728ac5a69d3b1f672857f3f1ef89e2c8
Jeff <jeffgp93@gmail.com>
2022-03-03 07:15:05 -0500

    Small changes to randomizer

--- dzmz/randomselect.py
+++ dzmz/randomselect.py
@@ -1,4 +1,5 @@
 import imp
+from math import factorial
 from random import choice, sample
 from dzmz.models import Card
 from dzmz import db
@@ -7,29 +8,29 @@ from random import sample, randint
 
 def randomizer():
     cards = Card.query.all()
-    try:
-        cardzero = sample(cards, 1)[0]
-    except TypeError as e:
-        print(cardzero)
-        raise e
+    cardzero = sample(cards, 1)[0]
     if cardzero.num_ratings < 10:
-        matches = Card.query.filter_by(
-            faction=cardzero.faction, type=cardzero.type
-        ).all()
+        if cardzero.type == "identity":
+            matches = Card.query.filter_by(type="identity", side=cardzero.side)
+        else:
+            matches = Card.query.filter_by(
+                faction=cardzero.faction, type=cardzero.type
+            ).all()
     elif cardzero.rating > 1200:
-        matches = Card.query.filter(Card.rating >= 800, faction=cardzero.faction).all()
-    elif cardzero.rating > 1800:
-        matches = Card.query.filter_by(side=cardzero.side).all()
+        if randint(0, 1):
+            matches = Card.query.filter(
+                Card.rating >= cardzero.rating - 400, Card.faction == cardzero.faction
+            ).all()
+        else:
+            matches = Card.query.filter(
+                Card.rating >= cardzero.rating - 400, Card.side == cardzero.side
+            ).all()
     else:
         if randint(0, 1):
             matches = Card.query.filter_by(faction=cardzero.faction).all()
         else:
             return randomizer()
-    try:
-        cardone = sample(matches, 1)[0]
-    except TypeError as e:
-        print(cardone)
-        raise e
+    cardone = sample(matches, 1)[0]
     if cardone.id == cardzero.id:
         return randomizer()
     return [cardzero, cardone]
