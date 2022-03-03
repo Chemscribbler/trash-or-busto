cece2ed6b93ba7ca52dfa4f2f7e0cef5bd42a6b8
Jeff <jeffgp93@gmail.com>
2022-03-02 20:08:20 -0500

    setup for gunicorn

--- app_dzmz.py
+++ app_dzmz.py
@@ -6,3 +6,7 @@ from dzmz.card_builder import build_card_db
 @app.shell_context_processor
 def make_shell_context():
     return {"db": db, "Card": Card, "Pair": Pair, "build_card_db": build_card_db}
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
